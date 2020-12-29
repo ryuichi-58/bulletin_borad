05d38c8bbc20ff0466787cd35d283f3ac2773ade
ryuichi-58 <ichi.aka.estab.ot@gmail.com>
2020-12-30 04:38:02 +0900

    投稿画面作成

---
+++ index.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<html lang="ja">
+<head>
+	<meta charset="UTF-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+	<meta http-equiv="X-UA-Compatible" content="ie=edge">
+	<title>ひとこと掲示板</title>
+
+	<link rel="stylesheet" href="style.css" />
+</head>
+
+<body>
+	<form action="" method="post">
+        <dl>
+            <dt>メッセージをどうぞ</dt>
+            <dd>
+                <textarea name="message" cols="50" rows="5"></textarea>
+            </dd>
+        </dl>
+        <div>
+            <input type="submit" value="投稿する">
+        </div>
+    </form>
+</body>
+</html>
