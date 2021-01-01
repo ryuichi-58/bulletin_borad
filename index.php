7361148d4548971ce59007317a759e17a8aa2c8d
ryuichi-58 <ichi.aka.estab.ot@gmail.com>
2021-01-01 21:04:28 +0900

    投稿を取得機能を追記

--- index.php
+++ index.php
@@ -31,6 +31,17 @@ if (!empty($_POST)) {
         exit();
     }
 }
+
+// 投稿を取得する
+$posts = $db->query('SELECT m.name, m.picture, p.* FROM members m, posts p WHERE m.id=p.member_id ORDER BY p.created DESC');
+// 返信の場合
+if (isset($_REQUEST['res'])) {
+    $response = $db->prepare('SELECT m.name, m.picture, p.* FROM members m.posts p WHERE m.id=p.member_id and p.id=? ORDER BY p.created DESC');
+    $response->execute(array($_REQUEST['res']));
+
+    $table = $response->fetch();
+    $message = '@' . $table['name'] . ' ' . $table['message'];
+}
 ?>
 
 <!DOCTYPE html>
@@ -54,13 +65,25 @@ if (!empty($_POST)) {
         <dl>
             <dt><?php echo htmlspecialchars($member['name'], ENT_QUOTES); ?>さん、メッセージをどうぞ</dt>
         <dd>
-        <textarea name="message" cols="50" rows="5"></textarea>
+        <textarea name="message" cols="50" rows="5"><?php echo htmlspecialchars($message, ENT_QUOTES); ?></textarea>
+        <input type="hidden" name="reply_post_id" value="<?php echo htmlspecialchars($_REQUEST['res'], ENT_QUOTES); ?>" />
         </dd>
         </dl>
         <div>
             <input type="submit" value="投稿する" />
         </div>
     </form>
+<?php
+foreach ($posts as $post):
+?>
+<div class="msg">
+    <img src="member_picture/<?php echo htmlspecialchars($post['picture'], ENT_QUOTES); ?>" width="48" height="48" alt="<?php echo htmlspecialchars($post['name'], ENT_QUOTES);?>" />
+    <p><?php echo htmlspecialchars($post['message'], ENT_QUOTES); ?><span class="name">(<?php echo htmlspecialchars($post['name'], ENT_QUOTES) ?>)</span>[<a href="index.php?res=<?php echo htmlspecialchars($post['id'], ENT_QUOTES); ?>">Re</a>]</p>
+    <p class="day"><?php echo htmlspecialchars($post['created'], ENT_QUOTES); ?></p>
+</div>
+<?php
+endforeach;
+?>
 </div>
 </body>
 </html>
