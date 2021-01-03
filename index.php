6773e72cd7b7573436f435a3f212b9589e20716e
ryuichi-58 <ichi.aka.estab.ot@gmail.com>
2021-01-03 09:13:27 +0900

    ページング機能を追記

--- index.php
+++ index.php
@@ -33,7 +33,24 @@ if (!empty($_POST)) {
 }
 
 // 投稿を取得する
-$posts = $db->query('SELECT m.name, m.picture, p.* FROM members m, posts p WHERE m.id=p.member_id ORDER BY p.created DESC');
+$page = $_REQUEST['page'];
+if ($page == '') {
+    $page = 1;
+}
+$page = max($page, 1);
+
+// 最終ページを取得する
+$counts = $db->query('SELECT COUNT(*) AS cnt FROM posts');
+$cnt = $counts->fetch();
+$maxPage = ceil($cnt['cnt'] / 5);
+$page = min($page, $maxPage);
+
+$start = ($page - 1) * 5;
+
+$posts = $db->prepare('SELECT m.name, m.picture, p.* FROM members m, posts p WHERE m.id=p.member_id ORDER BY p.created DESC LIMIT ?, 5');
+$posts->bindParam(1, $start, PDO::PARAM_INT);
+$posts->execute();
+
 // 返信の場合
 if (isset($_REQUEST['res'])) {
     $response = $db->prepare('SELECT m.name, m.picture, p.* FROM members m.posts p WHERE m.id=p.member_id and p.id=? ORDER BY p.created DESC');
@@ -45,6 +62,10 @@ if (isset($_REQUEST['res'])) {
 // hのショートカットキー
 function h($value) {
     return htmlentities($value, ENT_QUOTES);
+}
+// 本文内のURLにリンクを設定します
+function makeLink($value) {
+    return mb_ereg_replace("(https?)(://[[:alnum]\+\$\;\?\.%,!#~*/:@&=_-]+)", '<a href="\1\2">\1\2</a>', $value);
 }
 ?>
 
@@ -65,6 +86,7 @@ function h($value) {
     <h1>ひとこと掲示板</h1>
   </div>
   <div id="content">
+    <div style="text-align: right"><a href="logout.php">ログアウト</a></div>
     <form action="" method="post">
         <dl>
             <dt><?php echo h($member['name']); ?>さん、メッセージをどうぞ</dt>
@@ -82,7 +104,7 @@ foreach ($posts as $post):
 ?>
     <div class="msg">
         <img src="member_picture/<?php echo h($post['picture']); ?>" width="48" height="48" alt="<?php echo h($post['name']);?>" />
-        <p><?php echo h($post['message']); ?><span class="name">(<?php echo h($post['name']) ?>)</span>[<a href="index.php?res=<?php echo h($post['id']); ?>">Re</a>]</p>
+        <p><?php echo makeLink(h($post['message'])); ?><span class="name">(<?php echo h($post['name']) ?>)</span>[<a href="index.php?res=<?php echo h($post['id']); ?>">Re</a>]</p>
         <p class="day"><a href="view.php?id=<?php echo h($post['id']); ?>"><?php echo h($post['created']); ?></a>
     <?php
     if ($post['reply_post_id'] > 0):
@@ -91,11 +113,43 @@ foreach ($posts as $post):
     <?php
     endif;
     ?>
+    <?php
+    if ($_SESSION['id'] == $post['member_id']):
+    ?>
+        [<a href="delete.php?id=<?php echo h($post['id']); ?>" style="color:#F33;">削除</a>]
+    <?php
+    endif;
+    ?>
     </p>
     </div>
 <?php
 endforeach;
 ?>
+
+<ul class="paging">
+<?php
+if ($page > 1) {
+?>
+<li><a href="index.php?page=<?php print($page - 1); ?>">前のページへ</a></li>
+<?php
+} else {
+?>
+<li>前のページへ</li>
+<?php
+}
+?>
+<?php
+if ($page < $maxPage) {
+?>
+<li><a href="index.php?page=<?php print($page + 1); ?>">次のページへ</a></li>
+<?php
+} else {
+?>
+<li>次のページへ</li>
+<?php
+}
+?>
+</ul>
 </div>
 </body>
 </html>
--- login.php
+++ login.php
@@ -1,18 +1,17 @@
 <?php
-session_start();
 require('dbconnect.php');
 
+session_start();
 
-if ($_COOKIE['email'] !== '') {
+if ($_COOKIE['email'] != '') {
     $_POST['email'] = $_COOKIE['email'];
     $_POST['password'] = $_COOKIE['password'];
     $_POST['save'] = 'on';
 }
 
 if (!empty($_POST)) {
-    $email = $_POST['email'];
     // ログイン処理
-    if ($_POST['email'] !== '' && $_POST['password'] !== '') {
+    if ($_POST['email'] != '' && $_POST['password'] != '') {
         $login = $db->prepare('SELECT * FROM members WHERE email=? AND password=?');
         $login->execute(array(
             $_POST['email'],
@@ -25,11 +24,12 @@ if (!empty($_POST)) {
             $_SESSION['id'] = $member['id'];
             $_SESSION['time'] = time();
 
-                // ログイン情報を記録する
-                if ($_POST['save'] === 'on') {
-                    setcookie('email', $_POST['email'], time()+ 60 * 60 * 24 * 14);
-                    setcookie('password', $_POST['password'], time() + 60 * 60 * 24 * 14);
-                }
+            // ログイン情報を記録する
+            if ($_POST['save'] == 'on') {
+                setcookie('email', $_POST['email'], time()+ 60 * 60 * 24 * 14);
+                setcookie('password', $_POST['password'], time() + 60 * 60 * 24 * 14);
+            }
+
             header('Location: index.php');
             exit();
         } else {
@@ -41,15 +41,12 @@ if (!empty($_POST)) {
 }
 ?>
 
-<!DOCTYPE html>
-<html lang="ja">
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml">
 <head>
-	<meta charset="UTF-8">
-	<meta name="viewport" content="width=device-width, initial-scale=1.0">
-	<meta http-equiv="X-UA-Compatible" content="ie=edge">
-	<title>ひとこと掲示板</title>
-
-	<link rel="stylesheet" href="style.css" />
+<meta http-equiv="Content-Type" content="text/html; charset=UTF-8" />
+<link rel="stylesheet" type="text/css" href="style.css" />
+<title>ログインする</title>
 </head>
 
 <body>
@@ -58,28 +55,26 @@ if (!empty($_POST)) {
 			<h1>ログイン</h1>
 		</div>
 		<div id="content">
-			<div id="lead">
-			</div>
 			<form action="" method="post">
 				<dl>
 					<dt>メールアドレス</dt>
 					<dd>
-						<input type="text" name="email" size="35" maxlength="255" value="<?php echo htmlspecialchars($_POST['email'], ENT_QUOTES); ?>"/>
+						<input type="text" name="email" size="35" maxlength="255" value="<?php echo htmlspecialchars($_POST['email']); ?>"/>
 						<?php if ($error['login'] == 'blank'): ?>
-							<p class="error">* メールアドレスとパスワードをご記入ください</p>
+						<p class="error">* メールアドレスとパスワードをご記入ください</p>
 						<?php endif; ?>
 						<?php if ($error['login'] == 'failed'): ?>
-							<p class="error">* ログインに失敗しました。</p>
-						<?php endif; ?>
+						<p class="error">* ログインに失敗しました。</p>
+                        <?php endif; ?>
 					</dd>
 					<dt>パスワード</dt>
 					<dd>
-						<input type="password" name="password" size="35" maxlength="255" value="<?php echo htmlspecialchars($_POST['password'], ENT_QUOTES); ?>" />
+						<input type="password" name="password" size="35" maxlength="255" value="<?php echo htmlspecialchars($_POST['password']); ?>" />
 					</dd>
 					<dt>ログイン情報の記録</dt>
 					<dd>
-						<input id="save" type="checkbox" name="save" value="on"><label
-						for="save">次回からは自動的にログインする</label>
+                        <input id="save" type="checkbox" name="save" value="on">
+                        <label for="save">次回からは自動的にログインする</label>
 					</dd>
 				</dl>
 				<div><input type="submit" value="ログインする" /></div>
