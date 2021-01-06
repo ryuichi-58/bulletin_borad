d77eec1a12ae11e56df9541bdddbdd44716142c9
ryuichi-58 <ichi.aka.estab.ot@gmail.com>
2021-01-06 22:34:18 +0900

    UI変更に伴うhtml修正

--- view.php
+++ view.php
@@ -16,35 +16,41 @@ $posts->execute(array($_REQUEST['id']));
 <head>
 	<meta charset="UTF-8">
 	<meta name="viewport" content="width=device-width, initial-scale=1.0">
-	<meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <script src="https://kit.fontawesome.com/250c1a3838.js" crossorigin="anonymous"></script>
 	<title>ひとこと掲示板</title>
 
 	<link rel="stylesheet" href="style.css" />
 </head>
 
 <body>
-<div id="wrap">
-    <div id="head">
+    <div class="msg">
         <h1>ひとこと掲示板</h1>
-    </div>
-    <div id="content">
-    <p>&laquo;<a href="index.php">一覧に戻る</a></p>
-    <?php
-    if ($post = $posts->fetch()):
-    ?>
-        <div class="msg">
-        <img src="member_picture/<?php echo htmlspecialchars($post['picture'], ENT_QUOTES)?>" width="48" height="48" alt="<?php echo htmlspecialchars($post['name'], ENT_QUOTES); ?>" />
-        <p><?php echo htmlspecialchars($post['message'], ENT_QUOTES); ?><span class="name">(<?php echo htmlspecialchars($post['name'], ENT_QUOTES); ?>)</span></p>
-        <p class="day"><?php echo htmlspecialchars($post['created'], ENT_QUOTES); ?></p>
+        <div class="msg_wrapper">
+            <?php
+            if ($post = $posts->fetch()):
+            ?>
+            <div class="msg_container">
+                <img src="member_picture/<?php echo htmlspecialchars($post['picture'], ENT_QUOTES)?>" width="48" height="48" alt="<?php echo htmlspecialchars($post['name'], ENT_QUOTES); ?>" />
+                <article class="user">
+                <p><span class="name"><?php echo htmlspecialchars($post['name'], ENT_QUOTES); ?></span>
+                </article>
+                <article class="day">
+                <p class="created"><?php echo htmlspecialchars($post['created'], ENT_QUOTES); ?></p>
+                </article>
+            </div>
+                <article class="post">
+                <?php echo htmlspecialchars($post['message'], ENT_QUOTES); ?></p>
+                </article>
+                <?php
+                else:
+                ?>
+                <p>その投稿は削除されたか、URLが間違えています</p>
+                <?php
+                endif;
+                ?>
         </div>
-    <?php
-    else:
-    ?>
-        <p>その投稿は削除されたか、URLが間違えています</p>
-    <?php
-    endif;
-    ?>
+        <p class="paging"><a href="index.php"><i class="far fa-caret-square-left"></i> Back</a></p>
     </div>
-</div>
 </body>
 </html>
