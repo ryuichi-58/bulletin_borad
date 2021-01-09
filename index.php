54faef3a6fc9dbecb7cd7a9d32dd8daa0f766c4e
ryuichi-58 <ichi.aka.estab.ot@gmail.com>
2021-01-09 23:00:01 +0900

    いいね実装前に初期化

--- index.php
+++ index.php
@@ -74,9 +74,6 @@ function makeLink($value) {
     return mb_ereg_replace("(https?)(://[[:alnum]\+\$\;\?\.%,!#~*/:@&=_-]+)", '<a href="\1\2">\1\2</a>', $value);
 }
 
-if (!empty($_POST)) {
-
-}
 ?>
 <!DOCTYPE html>
 <html lang="ja">
